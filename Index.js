a66b46c166bbf0d46c89353294dc2cd56c64112d
GABRIEL CHEDIEX VINER <46916327@ort>
2023-04-03 10:40:40 -0300

    inicio del tp

--- Index.js
+++ Index.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+import { suma, resta, dividir, multiplicar } from './src/modules/matematica.js'
+
+
+
+function Ejemplo()
+{
+    let total;
+    let num1 = 8;
+    let num2 = 2;
+    console.log("Hello world");
+    
+    total = suma(num1,num2)
+    console.log(`la suma entre ${num1} y ${num2} es:  + ${total}`)
+    
+    total = resta(num1,num2)
+    console.log(`la resta entre ${num1} y ${num2} es:  + ${total}`)
+    
+    total = dividir(num1,num2)
+    console.log(`la division entre ${num1} y ${num2} es:  + ${total}`)
+    
+    total = multiplicar(num1,num2)
+    console.log(`la multiplicacion entre ${num1} y ${num2} es:  + ${total}`)
+}
+
---
+++ src/modules/matematica.js
@@ -0,0 +1,19 @@
+export const suma =(num1,num2) =>
+{ 
+    return num1+num2
+}
+
+export const resta =(num1,num2) =>
+{ 
+    return num1-num2
+}
+
+export const dividir =(num1,num2) =>
+{ 
+    return num1/num2
+}
+
+export const multiplicar =(num1,num2) =>
+{ 
+    return num1*num2
+}
